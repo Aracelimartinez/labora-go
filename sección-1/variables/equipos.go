ba5aa3022c22158987bc4ccaeeef97bb0fff21d2
Araceli Martinez <araceli_martinez94@hotmail.com>
2023-04-04 12:58:58 -0400

    Finish team exercise

---
+++ sección-1/variables/equipos.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	teamName := "1"
+	titles := 2
+	fmt.Printf("El equipo %s ha ganado %v títulos.\n", teamName, titles)
+
+	teamName = "2"
+	titles = 4
+	fmt.Printf("El equipo %s ha ganado %v títulos.\n", teamName, titles)
+
+	teamName = "3"
+	titles = 3
+	fmt.Printf("El equipo %s ha ganado %v títulos.\n", teamName, titles)
+
+	teamName = "4"
+	titles = 6
+	fmt.Printf("El equipo %s ha ganado %v títulos.\n", teamName, titles)
+
+	teamName = "5"
+	titles = 1
+	fmt.Printf("El equipo %s ha ganado %v títulos.\n", teamName, titles)
+}
---
+++ sección-1/variables/restaurantes.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	restaurantName := "Mac"
+	rating := 4.0
+	fmt.Printf("El restaurante %s tiene una calificación de %v estrellas.\n", restaurantName, rating)
+
+	restaurantName = "BK"
+	rating = 4.0
+	fmt.Printf("El restaurante %s tiene una calificación de %v estrellas.\n", restaurantName, rating)
+
+	restaurantName = "Subway"
+	rating = 4.5
+	fmt.Printf("El restaurante %s tiene una calificación de %v estrellas.\n", restaurantName, rating)
+
+}
