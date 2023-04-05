3e7eac2a35c80c890e3743dd474d802766151b5c
Araceli Martinez <araceli_martinez94@hotmail.com>
2023-04-05 19:29:25 -0400

    finish the 1 and 2 exercises from day 3

---
+++ sección-1/04-arrays/array/array.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	numbers := [10] int {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+	fmt.Println(numbers)
+
+	nombres := [] string {"Juan", "Pedro", "Luis"}
+
+	fmt.Println(nombres)
+}
---
+++ sección-1/04-arrays/peliculas/peliculas.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	films := [10] string {"El padrino", "Moana", "The Avengers", "Interestelar", "The Batman", "Los increíbles", "Megamente", "La mujer maravilla", "Superman", "Hannibal Lecter" }
+
+	fmt.Println("Las películas son:", films)
+	fmt.Println("La segunda película de la lista es:", films[1])
+	fmt.Println("La cantidad total de películas es:", len(films))
+
+
+}
