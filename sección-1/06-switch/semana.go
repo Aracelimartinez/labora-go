4bbfa3e224c174959281e2a388d1b50d5fc973c6
Araceli Martinez <araceli_martinez94@hotmail.com>
2023-04-10 19:27:45 -0400

    finish switch exercise

---
+++ sección-1/06-switch/semana.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+func main () {
+
+	num := askNumbers()
+	dayOfWeek(&num)
+}
+
+func askNumbers() (int) {
+	var num int
+
+	fmt.Printf("Ingresa un número del 1 al 7:")
+	fmt.Scanln(&num)
+
+	return num
+}
+
+
+func dayOfWeek(num *int)  {
+
+	switch *num {
+	case 1:
+	fmt.Printf("Lunes")
+
+	case 2:
+	fmt.Printf("Martes")
+
+	case 3:
+	fmt.Printf("Miércoles")
+
+	case 4:
+	fmt.Printf("Jueves")
+
+	case 5:
+	fmt.Printf("Viernes")
+
+	case 6:
+	fmt.Printf("Sábado")
+
+	case 7:
+	fmt.Printf("Domingo")
+
+	}
+
+}
